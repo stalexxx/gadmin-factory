919ca8d39bd2ab7bbe8a4e1dc78aa2ee119619d5
a.ostrovskiy <stalexxx@users.noreply.github.com>
2014-04-29 11:43:07 +0000

    Failed commit: Failed commit: Default

---
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/themes/superhero/SuperheroCellTableResources.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2012, i-Free. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+
+package com.ifree.common.gwt.client.themes.superhero;
+
+import com.ifree.common.gwt.client.themes.CellTableResources;
+import org.gwtbootstrap3.client.ui.CellTable;
+
+/**
+*
+* @author Alexander Ostrovskiy (a.ostrovskiy)
+* @since 28.03.14
+*/
+public interface SuperheroCellTableResources extends CellTableResources {
+
+    @Source("com/ifree/common/gwt/client/themes/superhero/CellTable.css")
+    CellTable.Style cellTableStyle();
+
+}
