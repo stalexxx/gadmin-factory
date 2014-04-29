7d98e393bf0f0d43eaaa5cdef737dd77d6d08ba4
a.ostrovskiy <a.ostrovskiy@7b23cb1c-1557-4eb3-b35b-89fa367623f7>
2014-04-29 11:46:18 +0000

    Failed commit: Failed commit: Default

---
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/themes/CellTableResources.java
@@ -0,0 +1,9 @@
+package com.ifree.common.gwt.client.themes;
+
+import org.gwtbootstrap3.client.ui.CellTable;
+
+/**
+ * Created by alex on 29.04.14.
+ */
+public interface CellTableResources extends CellTable.Resources {
+}
