54f8f6762b2105eebd8550e5069b03e6e6a82a1b
a.ostrovskiy <a.ostrovskiy@7b23cb1c-1557-4eb3-b35b-89fa367623f7>
2014-05-06 12:32:08 +0000

    [*] deliveries

---
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/data/CachingDataProxy.java
@@ -0,0 +1,30 @@
+package com.ifree.common.gwt.client.data;
+
+import com.google.common.collect.Maps;
+import com.google.gwt.core.client.Callback;
+import com.ifree.common.gwt.client.ui.grids.BaseDataProxy;
+import com.ifree.common.gwt.shared.loader.DataProxy;
+import com.ifree.common.gwt.shared.loader.PagingLoadResult;
+
+import java.util.Map;
+
+/**
+ * Created by alex on 05.05.14.
+ */
+public class CachingDataProxy implements DataProxy {
+    private Map<Class, BaseDataProxy> dataProxyMap = Maps.newHashMap();
+
+    public <T> void registerDataProxy(Class<T> clazz, BaseDataProxy<T> dataProxy) {
+        dataProxyMap.put(clazz, dataProxy);
+    }
+
+    public <C, R extends PagingLoadResult> void load(Class<R> type, Callback<R, Throwable> callback) {
+        BaseDataProxy<R> baseDataProxy = dataProxyMap.get(type);
+//        baseDataProxy.load(null, callback);
+    }
+
+    @Override
+    public void load(Object loadConfig, Callback callback) {
+
+    }
+}
