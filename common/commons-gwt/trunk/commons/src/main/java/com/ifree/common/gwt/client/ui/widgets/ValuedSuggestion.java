7f7af0d7f66c0987ba42932d0ebb4bb4195b53a1
a.ostrovskiy <stalexxx@users.noreply.github.com>
2014-05-13 16:08:19 +0000

    [*] suggest oracle

---
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/ui/widgets/ValuedSuggestion.java
@@ -0,0 +1,44 @@
+package com.ifree.common.gwt.client.ui.widgets;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+import com.google.gwt.user.client.ui.SuggestOracle;
+
+/**
+* Created by alex on 13.05.14.
+*/
+public class ValuedSuggestion<T> implements SuggestOracle.Suggestion, IsSerializable {
+    private T value;
+    private String displayString;
+    private String replacementString;
+
+    /**
+     * Constructor used by RPC.
+     */
+    public ValuedSuggestion() {
+    }
+
+    /**
+     * Constructor for <code>MultiWordSuggestion</code>.
+     *
+     * @param replacementString the string to enter into the SuggestBox's text
+     *          box if the suggestion is chosen
+     * @param displayString the display string
+     */
+    public ValuedSuggestion(T value, String replacementString, String displayString) {
+        this.value = value;
+        this.replacementString = replacementString;
+        this.displayString = displayString;
+    }
+
+    public String getDisplayString() {
+        return displayString;
+    }
+
+    public String getReplacementString() {
+        return replacementString;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
