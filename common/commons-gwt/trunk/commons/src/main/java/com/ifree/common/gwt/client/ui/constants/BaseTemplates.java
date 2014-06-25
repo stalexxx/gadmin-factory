9c6e60d426521518aa6fe15108512c5cd94b4193
a.ostrovskiy <a.ostrovskiy@7b23cb1c-1557-4eb3-b35b-89fa367623f7>
2014-06-25 15:12:16 +0000

    [*] 304

--- common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/ui/constants/BaseTemplates.java
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/ui/constants/BaseTemplates.java
@@ -14,13 +14,16 @@ import com.google.gwt.safehtml.shared.SafeHtml;
  */
 public interface BaseTemplates extends SafeHtmlTemplates {
 
-    @Template("<i class=\"{0} {1} {2}\" ></i>" )
+    @Template("<i class=\"{0} {1} {2}\"></i>" )
     SafeHtml icon(String fa, String icon, String color);
+
+    @Template("<i class=\"{0} {1} {2} {3}\" title=\"{4}\"></i>" )
+    SafeHtml icon(String fa, String icon, String size, String color, String title);
 
     @Template("<img class=\"{0}\" src=\"{1}\" style=\"height: 70px\"/>")
     SafeHtml image(String style, String url);
 
-    @Template("<span class=\"label {1}\" title=\"{2}\">{0}</span>")
+    @Template("<span class=\"label {1}\" title=\"{2}\" style=\"width: 100%\">{0}</span>")
     SafeHtml label(String text, String color, String title);
 
     @Template("<span class=\"badge\" style=\"margin: {1}px\">{0}</span>")
