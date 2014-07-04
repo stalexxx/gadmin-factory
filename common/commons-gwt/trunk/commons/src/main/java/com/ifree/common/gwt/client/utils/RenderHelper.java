760fe5d44cdecb9a14cfd5f850fd0b238b9595ca
a.ostrovskiy <stalexxx@users.noreply.github.com>
2014-07-04 08:26:51 +0000

    [*] orders

---
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/utils/RenderHelper.java
@@ -0,0 +1,30 @@
+package com.ifree.common.gwt.client.utils;
+
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+
+/**
+ * Created by alex on 03.07.14.
+ */
+public class RenderHelper {
+
+    public static SafeHtml multiline(String... list) {
+
+        SafeHtmlBuilder builder = new SafeHtmlBuilder();
+
+        if (list != null) {
+            for (String line : list) {
+
+                if (line != null) {
+
+                    builder.append(SafeHtmlUtils.fromString("<div>" + line + "</div>"));
+                }
+
+            }
+        }
+
+        return builder.toSafeHtml();
+
+    }
+}
