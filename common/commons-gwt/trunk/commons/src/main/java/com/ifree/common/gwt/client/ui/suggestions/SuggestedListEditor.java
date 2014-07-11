e3c5b57b456d2a28a9dc6be216fc0b930bde6f58
a.ostrovskiy <a.ostrovskiy@7b23cb1c-1557-4eb3-b35b-89fa367623f7>
2014-07-11 09:21:13 +0000

    [*] MOBS-537 MRetail: Web-UI: Не сохраняются девайсы

--- common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/ui/suggestions/SuggestedListEditor.java
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/ui/suggestions/SuggestedListEditor.java
@@ -2,23 +2,20 @@ package com.ifree.common.gwt.client.ui.suggestions;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.editor.client.IsEditor;
 import com.google.gwt.editor.client.LeafValueEditor;
-import com.google.gwt.event.dom.client.FocusEvent;
-import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.text.shared.Renderer;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasEnabled;
+import com.google.gwt.user.client.ui.HasValue;
 import com.ifree.common.gwt.client.ui.grids.BaseDataProxy;
 import com.ifree.common.gwt.client.ui.widgets.BagePanel;
 import com.ifree.common.gwt.shared.ValueProvider;
-import org.gwtbootstrap3.client.ui.TextBox;
 
 import java.util.List;
 import java.util.Set;
@@ -32,28 +29,18 @@ public class SuggestedListEditor<T> extends Composite implements LeafValueEditor
 
     /*===========================================[ STATIC VARIABLES ]=============*/
 
-    private static SuggestedListEditorUiBinder ourUiBinder = GWT.create(SuggestedListEditorUiBinder.class);
-    private final SuggestOracle oracle;
-
-    /*===========================================[ INSTANCE VARIABLES ]===========*/
-
-    @UiField(provided = true)
     BagePanel<T> bagePanel;
 
-    @UiField(provided = true)
-    SuggestBox suggestBox;
+    SuggestedEditor<T> suggestEditor;
 
     private Renderer<T> renderer;
-
 
     /*===========================================[ CONSTRUCTORS ]=================*/
 
     @Deprecated
     public SuggestedListEditor(Renderer<T> renderer) {
         this(renderer, null, null);
-
     }
-
 
     public SuggestedListEditor(Renderer<T> renderer, BaseDataProxy<T> dataProxy,
                                ValueProvider<T, String> searchField) {
@@ -61,93 +48,46 @@ public class SuggestedListEditor<T> extends Composite implements LeafValueEditor
         bagePanel = new BagePanel<T>(renderer);
         this.renderer = renderer;
 
-        if (dataProxy == null) {
-            oracle = new MultiWordSuggestOracle();
-        } else {
-            oracle = new BaseSuggestOracle<T>(dataProxy, renderer, searchField);
-        }
+        suggestEditor = new SuggestedEditor<T>(renderer, dataProxy, searchField);
 
-
-        final TextBox box = new TextBox();
-        suggestBox = new SuggestBox(oracle, box);
-        box.setStyleName("form-control");
-
-        box.addFocusHandler(new FocusHandler() {
+        suggestEditor.addSelectionHandler(new SelectionHandler<T>() {
             @Override
-            public void onFocus(FocusEvent event) {
-                suggestBox.showSuggestionList();
-            }
-        });
+            public void onSelection(SelectionEvent<T> event) {
+                suggestEditor.setValue(null);
 
-        suggestBox.addSelectionHandler(new SelectionHandler<SuggestOracle.Suggestion>() {
-            @Override
-            public void onSelection(SelectionEvent<SuggestOracle.Suggestion> event) {
                 final Set<T> value = Sets.newLinkedHashSet(getValue());
-                final T item;
-
-
-                if (event.getSelectedItem() instanceof Suggestion) {
-                    Suggestion<T> selectedItem = (Suggestion) event.getSelectedItem();
-                    item = selectedItem.getValue();
-                } else {
-                    throw new IllegalStateException();
-                    /*final String replacementString = event.getSelectedItem().getReplacementString();
-                    item = replacementMap.get(replacementString);
-                */
-                }
+                final T item = event.getSelectedItem();
 
                 if (item != null) {
                     value.add(item);
                     setValue(Lists.newArrayList(value), true);
                 }
-
-                suggestBox.setValue(null);
             }
         });
 
-        initWidget(ourUiBinder.createAndBindUi(this));
+        FlowPanel panel = new FlowPanel();
+        panel.add(suggestEditor);
+        panel.add(bagePanel);
+
+        initWidget(panel);
     }
 
 
     /*===========================================[ INTERFACE METHODS ]============*/
-
-    /*public void setAcceptableValues(List<T> acceptableValues) {
-        if (acceptableValues != null) {
-            MultiWordSuggestOracle oracle = (MultiWordSuggestOracle) this.oracle;
-            oracle.clear();
-            replacementMap.clear();
-
-            for (T acceptableValue : acceptableValues) {
-                final String render = renderer.render(acceptableValue);
-                oracle.add(render);
-                replacementMap.put(render, acceptableValue);
-            }
-
-
-            oracle.setDefaultSuggestionsFromText(Lists.transform(acceptableValues, new Function<T, String>() {
-                @Nullable
-                @Override
-                public String apply(@Nullable T input) {
-                    return input != null ? renderer.render(input) : null;
-                }
-            }));
-        }
-    }*/
-
-    @Override
-    public void setValue(List<T> value) {
-        setValue(value, false);
-    }
 
     @Override
     public void setValue(List<T> value, boolean fireEvents) {
         bagePanel.setValue(value, fireEvents);
     }
 
-
     @Override
     public List<T> getValue() {
         return bagePanel.getValue();
+    }
+
+    @Override
+    public void setValue(List<T> value) {
+        setValue(value, false);
     }
 
     @Override
@@ -162,19 +102,17 @@ public class SuggestedListEditor<T> extends Composite implements LeafValueEditor
 
     @Override
     public boolean isEnabled() {
-        return suggestBox.isEnabled();
+        return suggestEditor.isEnabled();
     }
 
     @Override
     public void setEnabled(boolean enabled) {
-        suggestBox.setEnabled(enabled);
+        suggestEditor.setEnabled(enabled);
         bagePanel.setEnabled(enabled);
     }
 
     /*===========================================[ INNER CLASSES ]================*/
 
-    interface SuggestedListEditorUiBinder extends UiBinder<HTMLPanel, SuggestedListEditor> {
-    }
 
 
 }
