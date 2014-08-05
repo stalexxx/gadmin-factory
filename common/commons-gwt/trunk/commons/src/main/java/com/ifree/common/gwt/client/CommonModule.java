de46376ee0eaddb117e7b1fd9eabab8a68ab9d0e
a.ostrovskiy <a.ostrovskiy@7b23cb1c-1557-4eb3-b35b-89fa367623f7>
2014-08-05 10:09:22 +0000

    [*] 700

--- common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/CommonModule.java
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/CommonModule.java
@@ -1,6 +1,9 @@
 package com.ifree.common.gwt.client;
 
 import com.google.gwt.inject.client.AbstractGinModule;
+import com.google.inject.Singleton;
+import com.ifree.common.gwt.client.data.StorageService;
+import com.ifree.common.gwt.client.ui.application.security.CurrentUser;
 import com.ifree.common.gwt.client.ui.constants.BaseMessages;
 import com.ifree.common.gwt.client.ui.constants.BaseTemplates;
 
@@ -12,6 +15,9 @@ public class CommonModule extends AbstractGinModule {
     protected void configure() {
         bind(BaseMessages.class).asEagerSingleton();
         bind(BaseTemplates.class).asEagerSingleton();
+
+        bind(CurrentUser.class).in(Singleton.class);
+        bind(StorageService.class).in(Singleton.class);
 
         //resolvers
         //bind(ViewHeaderResolver.class).asEagerSingleton();
