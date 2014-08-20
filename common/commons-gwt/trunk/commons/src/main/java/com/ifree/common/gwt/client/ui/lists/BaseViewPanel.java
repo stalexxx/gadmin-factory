724e635d4cf358574357a5ec786f23db7b0f9794
a.ostrovskiy <a.ostrovskiy@7b23cb1c-1557-4eb3-b35b-89fa367623f7>
2014-08-20 15:07:23 +0000

    [*] respositive

--- common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/ui/lists/BaseViewPanel.java
+++ common/commons-gwt/trunk/commons/src/main/java/com/ifree/common/gwt/client/ui/lists/BaseViewPanel.java
@@ -2,48 +2,69 @@ package com.ifree.common.gwt.client.ui.lists;
 
 import com.google.gwt.user.cellview.client.AbstractPager;
 import com.google.gwt.user.client.ui.Widget;
+import com.ifree.common.gwt.client.ui.BaseFilter;
 import com.ifree.common.gwt.client.ui.BaseToolbar;
-import com.ifree.common.gwt.client.ui.application.Filter;
 import com.ifree.common.gwt.client.ui.grids.BaseListGrid;
 import org.gwtbootstrap3.client.ui.Column;
 import org.gwtbootstrap3.client.ui.Container;
 import org.gwtbootstrap3.client.ui.Row;
 import org.gwtbootstrap3.client.ui.constants.ColumnSize;
+import org.gwtbootstrap3.client.ui.constants.NavbarType;
 import org.gwtbootstrap3.client.ui.gwt.FlowPanel;
 
 /**
 * Created by alex on 14.05.14.
 */
-public class BaseViewPanel<T, F extends Filter> extends FlowPanel {
+public class BaseViewPanel<T, F extends BaseFilter> extends FlowPanel {
 
-    private final Row row;
-    public BaseViewPanel(BaseListGrid<T, F> dataGrid, BaseToolbar toolbar) {
+    private final Row footerRow;
+    private final Row mainRow;
+
+    private final Column gridColumn;
+
+
+    public BaseViewPanel(BaseListGrid<T, F> dataGrid, BaseToolbar toolbar, BaseFilterPanel<F, ? extends BaseFilterPanel> filterPanel) {
+
+        Container container = new Container();
+        container.setFluid(true);
+        container.add(mainRow = new Row());
+
         add(toolbar);
-        add(dataGrid);
+        add(container);
 
-        Container footer = new Container();
-        footer.setFluid(true);
+        gridColumn = new Column(ColumnSize.LG_10, ColumnSize.MD_10, ColumnSize.SM_12);
+        gridColumn.add(dataGrid);
+        mainRow.add(gridColumn);
 
-        add(footer);
+        if (filterPanel != null) {
+            Column filterPanelContainer = new Column(ColumnSize.LG_2,ColumnSize.MD_2, ColumnSize.SM_6);
 
-        footer.add(row = new Row());
+            filterPanelContainer.addStyleName(NavbarType.DEFAULT.getCssName());
+            filterPanelContainer.add(filterPanel);
+            mainRow.add(filterPanelContainer);
+
+        }
+
+        footerRow = new Row();
+        gridColumn.add(footerRow);
+
     }
 
     public void addPager(AbstractPager pager) {
         if (pager != null) {
-            row.add(wrap(pager, ColumnSize.MD_11));
+            footerRow.add(wrap(pager, ColumnSize.MD_10, ColumnSize.SM_9, ColumnSize.LG_11, ColumnSize.XS_9));
         }
     }
 
     public void addPageSizeWidget(Widget pageSizeWidget) {
         if (pageSizeWidget != null) {
             pageSizeWidget.addStyleName("pagination");
-            row.add(wrap(pageSizeWidget, ColumnSize.MD_1));
+            footerRow.add(wrap(pageSizeWidget, ColumnSize.MD_2, ColumnSize.SM_3, ColumnSize.LG_1, ColumnSize.XS_3));
         }
     }
 
-    private Column wrap(Widget widget, ColumnSize md8) {
-        Column pagerCol = new Column(md8);
+    private Column wrap(Widget widget, ColumnSize md8, ColumnSize ... elseSize) {
+        Column pagerCol = new Column(md8, elseSize);
         pagerCol.add(widget);
 
         return pagerCol;
